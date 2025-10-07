44f41ebd4a4d53259873c2777b84e3f93bdb3ea4
suleyj <suleyj101@gmail.com>
2025-10-07 18:12:26 -0400

    feat: add initial migration file
    - Add the first migration file for database setup
    - Remove unnecessary raw SQL files from repo

---
+++ server/db/migrations/20251007220243_init_tables.sql
@@ -0,0 +1,40 @@
+-- migrate:up
+CREATE SCHEMA feedback_board;
+
+CREATE TABLE feedback_board.users (
+  id SERIAL PRIMARY KEY,
+  fullname VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(350) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE feedback_board.feedback (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES feedback_board.users(id) ON DELETE CASCADE,
+  status VARCHAR(50) NOT NULL,
+  title VARCHAR(70) UNIQUE NOT NULL,
+  category VARCHAR(50) NOT NULL,
+  details VARCHAR(350) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE feedback_board.upvotes (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES feedback_board.users(id) ON DELETE CASCADE,
+  feedback_id INTEGER REFERENCES feedback_board.feedback(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT NOW(),
+  CONSTRAINT unique_like_user_feedback UNIQUE (user_id, feedback_id)
+);
+
+CREATE TABLE feedback_board.comments (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES feedback_board.users(id) ON DELETE CASCADE,
+  feedback_id INTEGER REFERENCES feedback_board.feedback(id) ON DELETE CASCADE,
+  comment_text VARCHAR(255),
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+-- migrate:down
+DROP SCHEMA feedback_board CASCADE;
