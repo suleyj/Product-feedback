c1a50eefa94256879af809a58effc8d0d52492b1
suleyj <suleyj101@gmail.com>
2025-12-15 09:11:42 -0500

    update vite config for prod

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/',
+    build: {
+        outDir: '../build'
+    },
     plugins: [
         react({
             babel: {
